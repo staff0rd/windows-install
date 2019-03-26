cc8a2f20303265a31a665c8099aadd252f07c1e0
Stafford Williams <stafford.williams@gmail.com>
2019-03-27 09:45:17 +1100

    more stuffs

--- ChocoFreshWindows.ps1
+++ ChocoFreshWindows.ps1
@@ -7,8 +7,10 @@ iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 choco install powershell -y
 choco install googlechrome -y
 choco install dashlane -y
-## choco install firefox -y
+choco install firefox -y
 ## choco install 7zip -y
+choco install cmder -y
+choco install executor -y
 
 ## Install Developer Tools
 choco install git -y
@@ -16,7 +18,7 @@ choco install nodejs -y
 choco install visualstudiocode -y
 choco install dotnetcore-sdk -y
 choco install microsoft-teams -y
-## choco install slack -y
+choco install slack -y
 choco install visualstudio2017professional -y
 choco install azure-cli -y
 choco install docker -y
