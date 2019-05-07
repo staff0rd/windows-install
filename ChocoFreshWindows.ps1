22fe707d66cce21cc9c97da828b20b49b212e244
Stafford Williams <stafford.williams@gmail.com>
2019-05-07 11:55:21 +1000

    fix for 64bit

--- ChocoFreshWindows.ps1
+++ ChocoFreshWindows.ps1
@@ -36,8 +36,8 @@ choco install sql-server-management-studio -y
 choco install beyondcompare -y
 
 # configure git for beyond compare
-git config --global diff.tool bc3
-git config --global merge.tool bc3
+git config --global difftool.bc3.path "c:/Program Files/Beyond Compare 4/bcomp.exe"
+git config --global mergetool.bc3.path "c:/Program Files/Beyond Compare 4/bcomp.exe"
 git config --global mergetool.bc3.trustExitCode true
 
 # won't work because npm is not in path
