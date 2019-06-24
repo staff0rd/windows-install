cfe9fa9869ffff49c874ecf46a0fb8e4da3c3814
Stafford Williams <stafford.williams@gmail.com>
2019-06-24 19:45:40 +1000

    slack, disk-inventory-x

--- BrewFreshMac.sh
+++ BrewFreshMac.sh
@@ -4,3 +4,5 @@ brew cask install visual-studio-code
 brew cask install docker
 brew install azure-cli
 brew cask install azure-data-studio
+brew cask install slack
+brew cask install disk-inventory-x
