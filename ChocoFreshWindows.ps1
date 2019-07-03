479e0811c6ceab27c7725777abc3b0e5d2601571
stafford <stafford.williams@gmail.com>
2019-07-03 22:09:25 +1000

    vlc

--- ChocoFreshWindows.ps1
+++ ChocoFreshWindows.ps1
@@ -4,32 +4,34 @@
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 
 ## Install Desktop tools
-choco install powershell -y
-choco install googlechrome -y
-choco install dashlane -y
-choco install firefox -y
-## choco install 7zip -y
-choco install cmder -y
-choco install executor -y
-choco install whois -y
-choco install sendtokindle -y
+cinst powershell -y
+cinst googlechrome -y
+cinst dashlane -y
+cinst firefox -y
+## cinst 7zip -y
+cinst vlc -y
+cinst cmder -y
+cinst executor -y
+cinst whois -y
+cinst sendtokindle -y
 
 ## Install Developer Tools
-choco install git -y
-choco install nodejs -y
-choco install visualstudiocode -y
-choco install dotnetcore-sdk -y
-choco install dotnetcore -y
-choco install microsoft-teams -y
-choco install slack -y
-choco install visualstudio2017professional -y
-choco install azure-cli -y
-choco install azurepowershell -y
-choco install checksum -y
-choco install docker-desktop -y
-choco install docker-cli -y
-choco install docker-compose -y
-choco install sql-server-management-studio -y
+cinst git -y
+cinst nodejs -y
+cinst visualstudiocode -y
+cinst dotnetcore-sdk -y
+cinst dotnetcore -y
+cinst microsoft-teams -y
+cinst slack -y
+cinst visualstudio2017professional -y
+cinst azure-cli -y
+cinst azurepowershell -y
+cinst checksum -y
+cinst docker-desktop -y
+cinst docker-cli -y
+cinst docker-compose -y
+cinst sql-server-management-studio -y
 
+## restart shell before running...
 npm install -g @angular/cli
 npm install -g --production windows-build-tools
