59c089cd94ed7f157b9683058e1d32f41212d3f3
Stafford Williams <stafford.williams@gmail.com>
2019-08-31 12:24:47 +1000

    node + dotnetsdk

--- BrewFreshMac.sh
+++ BrewFreshMac.sh
@@ -7,3 +7,7 @@ brew cask install azure-data-studio
 brew cask install slack
 brew cask install disk-inventory-x
 brew cask install spectacle
+brew tap azure/functions
+brew install azure-functions-core-tools
+brew install node
+brew cask install dotnet-sdk
